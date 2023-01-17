ca38c90f6ba3196a5083baeb4bce2ee5ddf5716c
Daniel Nowrot <daniel.nowrot@gmail.com>
2023-01-17 14:19:45 +0100

    changed classNames

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,18 +1,19 @@
 import "./style.css";
 
+
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-    <ul className="tasks__list">
+    <ul className="tasks">
         {tasks.map(task => (
             <li
                 key={task.id}
-                className={`tasks__listItem ${hideDone && task.done ? "tasks__listItem--hidden" : ""}`}
+                className={`tasks__item ${hideDone && task.done ? "tasks__item--hidden" : ""}`}
             >
                 <button 
                     className="task__button task__button--accept"
                     onClick={()=>toggleTaskDone(task.id)}>
                     {task.done ? "✓" : " "}
                 </button>
-                <span className={`tasks__listText ${task.done ? "tasks__listText--done" : ""}`}>
+                <span className={`tasks__content ${task.done ? "tasks__content--done" : ""}`}>
                     {task.content}
                 </span>
                 <button
@@ -23,6 +24,7 @@ const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
             </li >
         ))}
     </ul >
+    
 );
 
 export default Tasks;
