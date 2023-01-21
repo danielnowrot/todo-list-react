3a19b49ad4aa6c8685666faf437bb3089a16ffa6
Daniel Nowrot <daniel.nowrot@gmail.com>
2023-01-21 17:29:31 +0100

    added useTasks hook

---
+++ src/useTasks.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+export function useTasks() {
+
+    const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasksTable")) ?? []);
+
+    useEffect(() => {
+        localStorage.setItem("tasksTable", JSON.stringify(tasks));
+    }, [tasks]);
+
+    const removeTask = (id) => {
+        localStorage.setItem("tasksTable", JSON.stringify(tasks));
+        setTasks(tasks => tasks.filter(task => task.id !== id));
+    };
+
+    const toggleTaskDone = (id) => {
+        setTasks(tasks => tasks.map(task => task.id === id ?
+            { ...task, done: !task.done } : task
+        ));
+    };
+
+    const setAllDone = () => {
+        setTasks(tasks => tasks.map(task => ({ ...task, done: true })));
+    };
+
+    const addNewTask = (newTaskContent) => {
+        setTasks(tasks => [
+            ...tasks,
+            {
+                content: newTaskContent,
+                done: false,
+                id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+            }]);
+        localStorage.setItem("tasksTable", JSON.stringify(tasks));
+    };
+    return ({ tasks, removeTask, toggleTaskDone, setAllDone, addNewTask, })
+}
