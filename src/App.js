b00e64fb3c940d4e8889b0858e135c5bfb33a90a
Daniel Nowrot <daniel.nowrot@gmail.com>
2023-02-26 19:06:47 +0100

    added style to navigation

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import AuthorPage from "./features/author/AuthorPage";
-import { StyledNavLink } from "./styled";
+import { List, StyledNavLink } from "./styled";
 
 export const App = () => (
     <HashRouter>
         <nav>
-            <ul>
+            <List>
                 <li>
                     <StyledNavLink to="/zadania">Zadania</StyledNavLink>
                 </li>
                 <li>
                     <StyledNavLink to="/autor">O autorze</StyledNavLink>
                 </li>
-            </ul>
+            </List>
             <Switch>
             <Route path="/zadania/:id">
                     <TaskPage/>
--- src/styled.js
+++ src/styled.js
@@ -2,7 +2,24 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const StyledNavLink = styled(NavLink)`
+    margin: 10px;
+    padding: 20px;
+    color: white;
+    text-decoration: none;
+    
     &.active {
-        color:red;
+        color: white;
+        font-weight:bold ;
     }
+`;
+
+export const List = styled.ul`
+    list-style-type: none;
+    display: flex ;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    background-color: teal;
+    margin: 0;
+    padding: 10px;
 `;
